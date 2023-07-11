5c6ef7306ca2fa759f5b344e755f502f5a1ee245
Fehér Tamás <tamas.feher.tf@hotmail.com>
2023-07-11 16:37:49 +0200

    Add  guard

---
+++ Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionExtensions
+{
+    public class Guard
+    {
+        public static void Require(bool precondition, string exceptionMessage)
+        {
+            if (!precondition)
+                throw new ArgumentException(exceptionMessage);
+        }
+    }
+}
---
+++ ValueObjects/SimpleValueObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionExtensions.ValueObjects
+{
+    [Serializable]
+    public abstract class SimpleValueObject<T> : ValueObject
+       where T : IComparable
+    {
+        public T Value { get; }
+
+        protected SimpleValueObject(T value)
+        {
+            Value = value;
+        }
+
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString();
+        }
+
+        public static implicit operator T(SimpleValueObject<T> valueObject)
+        {
+            return valueObject == null ? default : valueObject.Value;
+        }
+    }
+}
